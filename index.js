ddc5994b63ed6ad3789c1a0707c214db9db897bd
Robby Costales <rcostales15@simons-rock.edu>
2019-05-11 18:19:11 -0400

    imported zip.js, added data for testing

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ predict_button.addEventListener("click", ()=>{
 });
 
 predict.addEventListener("change", ()=>{
-    var file    = document.querySelector('input[type=file]').files[0];
+    // var file = document.querySelector('input[type=file]').files[0];
+
+    // var zip = new JSZip();
+    //   zip.loadAsync( document.querySelector('input[type=file]').files[0] /* = file blob */)
+    //      .then(function(zip) {
+    //          // process ZIP file content here
+    //          alert("OK")
+    //      }, function() {alert("Not a valid zip file")});
+    //
+    // console.log(zip)
+    // console.log(typeof zip)
+
     var reader  = new FileReader();
     var img = document.getElementById('predict_image');
+
+    if (file) {
+        reader.readAsDataURL(file); //reads the data as a URL
+    }
 
     reader.onloadend = function () {
         img.src = reader.result;
@@ -25,14 +40,12 @@ predict.addEventListener("change", ()=>{
                 p.innerHTML = pred.className + " " + pred.probability;
                 document.getElementById("pred_output").appendChild(p);
             }
-            
+
             });
         });
-        
+
     }
 
-    if (file) {
-        reader.readAsDataURL(file); //reads the data as a URL
-    }
+
 
 });
